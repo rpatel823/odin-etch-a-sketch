2a7ba3320a1518db56c80dec518c9d1fb3ba5492
Raj <r.patel823@yahoo.com>
2024-12-09 21:54:45 -0500

    Implemented color changing
    Squares can change colors when hovered over!

--- etch-sketch.js
+++ etch-sketch.js
@@ -7,8 +7,11 @@ function createSquare()
     //create a div
     const square = document.createElement("div");
 
-    //give it same width and height (50px) and make it orange
-    square.style.cssText = "width: 50px; height: 50px; background: orange; margin: 5px;"
+    //add a class to the div
+    square.classList.add('square');
+
+    //give it same width and height (50px) and initial color
+    square.style.cssText = "width: 50px; height: 50px; background-color: green; margin: 5px;"
 
     //attach it to the existing container div
     squareContainer.appendChild(square);
@@ -19,3 +22,16 @@ for (let i = 0; i < NUM_OF_SQUARES; i++)
 {
         createSquare();
 }
+
+function changeColor (event)
+{
+    event.target.style.backgroundColor = "purple";
+}
+
+//changes square color when mouse is over the square
+squareContainer.addEventListener('mouseover', (event) => {
+    if (event.target.classList.contains('square'))
+    {
+        changeColor(event);
+    }
+  });
