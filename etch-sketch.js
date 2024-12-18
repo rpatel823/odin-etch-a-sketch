e4cdd3967d6f1ebe6506dc4065ec40392ef7d023
Raj <r.patel823@yahoo.com>
2024-12-18 10:59:49 -0500

    Implemented make new grid button
    Can now change grid size based on user prompt
    (ex: 5x5 grid or a 100x100 grid)

--- etch-sketch.js
+++ etch-sketch.js
@@ -1,27 +1,52 @@
 //global variables
 const squareContainer = document.querySelector("#squareContainer");
-let NUM_OF_SQUARES = 256;
+const buttonContainer = document.querySelector(".newGridButton");
+//700 container - 80 left/right padding = 620
+const USABLE_CONTAINER_SPACE = 620;
+//initial grid of 16x16;
+let gridSize = 16;
+let squareSize = 0;
 
 function createSquare()
 {
-    //create a div
+    //create a div to represent a sqaure
     const square = document.createElement("div");
 
-    //add a class to the div
+    //add a class to the div for styling
     square.classList.add('square');
 
-    //give it same width and height (50px) and initial color
-    square.style.cssText = "width: 50px; height: 50px; background-color: green; margin: 5px;"
+    //compute the square size to fit grid across all usuable space;
+    squareSize = USABLE_CONTAINER_SPACE/gridSize;
+
+    //update the square's width/height to that size;
+    square.style.flexBasis = squareSize.toString() + "px";
+    square.style.height = squareSize.toString() + "px";
 
     //attach it to the existing container div
     squareContainer.appendChild(square);
 }
 
-//creates grid of squares
-for (let i = 0; i < NUM_OF_SQUARES; i++)
+function deleteGrid()
 {
-        createSquare();
+    //html collection of the current square elements
+    let squares = document.getElementsByClassName('square');
+
+    //iterate through collection backwards and remove each element
+    for (let i = (squares.length - 1); i >= 0; i--) 
+    {
+        squares[i].parentNode.removeChild(squares[i]);
+    }
 }
+ 
+
+function createGrid(gridSize)
+{
+    for (let i = 0; i < (gridSize * gridSize); i++)
+        {
+            createSquare();
+        }
+}
+    
 
 function changeColor (event)
 {
@@ -29,9 +54,30 @@ function changeColor (event)
 }
 
 //changes square color when mouse is over the square
-squareContainer.addEventListener('mouseover', (event) => {
-    if (event.target.classList.contains('square'))
+squareContainer.addEventListener('mouseover', (event) =>
     {
-        changeColor(event);
+        if (event.target.classList.contains('square'))
+        {
+            changeColor(event);
+        }
+    });
+
+//replace current grid with new grid based off user input
+buttonContainer.addEventListener("click", (event) =>
+{
+    gridSize = prompt("What's the new grid size (max 100)?");
+    gridSize = Number(gridSize);
+
+    //input validation check
+    while (gridSize <= 0 || gridSize > 100 || isNaN(gridSize))
+    {
+        gridSize = prompt("Please enter a valid grid size (max 100)");
+        gridSize = Number(gridSize);
     }
-  });
+
+    deleteGrid();
+    createGrid(gridSize);
+});
+
+//initial grid creation
+createGrid(gridSize);
